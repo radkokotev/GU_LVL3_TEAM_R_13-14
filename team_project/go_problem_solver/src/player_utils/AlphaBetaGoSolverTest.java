1bc193126a9d9fb213708b2a7d2f6a59e71d7aaf
Radko Kotev <radkokotev@gmail.com>
2013-11-26 16:16:59 +0000

    Alpha-Beta pruning on top of the current minimax implementation.

---
+++ team_project/go_problem_solver/src/player_utils/AlphaBetaGoSolverTest.java
@@ -0,0 +1,69 @@
+package player_utils;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import custom_java_utils.CheckFailException;
+
+import board_utils.GoCell;
+import board_utils.GoPlayingBoard;
+import board_utils.Stone;
+
+public class AlphaBetaGoSolverTest {
+	
+	@Before
+	public void setUp() throws Exception {
+		// Reset the history after every test case;
+		BoardHistory.wipeHistory();
+	}
+	@Test
+	public void testIsPositionTerminal() throws FileNotFoundException, CheckFailException {
+		GoPlayingBoard board = new GoPlayingBoard(
+				"src/player_utils/test_data/small_board_for_minimax");
+		AlphaBetaGoSolver solver = new AlphaBetaGoSolver(board, board.getCellAt(18, 18));
+		assertFalse(solver.isPositionTerminal(board));
+		board.setCellAt(18, 18, new GoCell(Stone.BLACK, 18, 17));
+		board.setCellAt(18, 18, new GoCell(Stone.NONE, 18, 18));
+		assertTrue(solver.isPositionTerminal(board));
+	}
+
+	@Test
+	public void testMinimaxDecisionBlack() throws FileNotFoundException, CheckFailException {
+		GoPlayingBoard board = new GoPlayingBoard(
+				"src/player_utils/test_data/small_board_for_minimax");
+		board.setToPlayNext(Stone.BLACK);
+		AlphaBetaGoSolver solver = new AlphaBetaGoSolver(board, board.getCellAt(18, 18));
+		assertEquals(new GoCell(Stone.BLACK, 18, 17), solver.minimaxDecision());
+	}
+	
+	@Test
+	public void testMinimaxDecisionWhite() throws FileNotFoundException, CheckFailException {
+		GoPlayingBoard board = new GoPlayingBoard(
+				"src/player_utils/test_data/small_board_for_minimax_white");
+		board.setToPlayNext(Stone.WHITE);
+		AlphaBetaGoSolver solver = new AlphaBetaGoSolver(board, board.getCellAt(18, 18));
+		assertEquals(new GoCell(Stone.WHITE, 18, 17), solver.minimaxDecision());
+	}
+	
+	@Test
+	public void testMinimaxDecisionUnsettled3White() throws FileNotFoundException, CheckFailException {
+		GoPlayingBoard board = new GoPlayingBoard(
+				"src/player_utils/test_data/unsettled_three_white");
+		board.setToPlayNext(Stone.WHITE);
+		AlphaBetaGoSolver solver = new AlphaBetaGoSolver(board, board.getCellAt(14, 1));
+		assertEquals(new GoCell(Stone.WHITE, 14, 0), solver.minimaxDecision());
+	}
+	
+	@Test
+	public void testMinimaxDecisionUnsettled3Black() throws FileNotFoundException, CheckFailException {
+		GoPlayingBoard board = new GoPlayingBoard(
+				"src/player_utils/test_data/unsettled_three_black");
+		board.setToPlayNext(Stone.BLACK);
+		AlphaBetaGoSolver solver = new AlphaBetaGoSolver(board, board.getCellAt(14, 1));
+		assertEquals(new GoCell(Stone.BLACK, 14, 0), solver.minimaxDecision());
+	}
+}
