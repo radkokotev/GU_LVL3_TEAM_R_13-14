3290d6d6b2d0b3e9ed33d2fe2013aaf2680a0e99
Radko Kotev <radkokotev@gmail.com>
2014-02-17 13:59:56 +0000

    Better multithreaded implementation. No exdplicit checks for race conditions.

--- team_project/go_problem_solver/src/player_utils/MonteCarloGoSolverTest.java
+++ team_project/go_problem_solver/src/player_utils/MonteCarloGoSolverTest.java
@@ -38,7 +38,7 @@ public class MonteCarloGoSolverTest {
 	}
 
 	@Test
-	public void testMinimaxDecisionBlack() throws FileNotFoundException, CheckFailException, InterruptedException {
+	public void testMonteCarloDecisionBlack() throws FileNotFoundException, CheckFailException, InterruptedException {
 		GoPlayingBoard board = new GoPlayingBoard(workspaceDirectory +
 				"/src/player_utils/test_data/small_board_for_minimax");
 		board.setToPlayNext(Stone.BLACK);
@@ -47,7 +47,7 @@ public class MonteCarloGoSolverTest {
 	}
 	
 	@Test
-	public void testMinimaxDecisionWhite() throws FileNotFoundException, CheckFailException, InterruptedException {
+	public void testMonteCarloDecisionWhite() throws FileNotFoundException, CheckFailException, InterruptedException {
 		GoPlayingBoard board = new GoPlayingBoard(workspaceDirectory +
 				"/src/player_utils/test_data/small_board_for_minimax_white");
 		board.setToPlayNext(Stone.WHITE);
@@ -56,7 +56,7 @@ public class MonteCarloGoSolverTest {
 	}
 	
 	@Test
-	public void testMinimaxDecisionUnsettled3White() throws FileNotFoundException, CheckFailException, InterruptedException {
+	public void testMonteCarloDecisionUnsettled3White() throws FileNotFoundException, CheckFailException, InterruptedException {
 		GoPlayingBoard board = new GoPlayingBoard(workspaceDirectory +
 				"/src/player_utils/test_data/unsettled_three_white");
 		board.setToPlayNext(Stone.WHITE);
@@ -65,7 +65,7 @@ public class MonteCarloGoSolverTest {
 	}
 	
 	@Test
-	public void testMinimaxDecisionUnsettled3Black() throws FileNotFoundException, CheckFailException, InterruptedException {
+	public void testMonteCarloDecisionUnsettled3Black() throws FileNotFoundException, CheckFailException, InterruptedException {
 		GoPlayingBoard board = new GoPlayingBoard(workspaceDirectory +
 				"/src/player_utils/test_data/unsettled_three_black");
 		board.setToPlayNext(Stone.BLACK);
@@ -74,12 +74,21 @@ public class MonteCarloGoSolverTest {
 	}
 	
 	@Test
-	public void testMinimaxProblem1FromHandout() throws FileNotFoundException, CheckFailException, InterruptedException {
+	public void testProblem1FromHandout() throws FileNotFoundException, CheckFailException, InterruptedException {
 		GoPlayingBoard board = new GoPlayingBoard(workspaceDirectory +
 				"/src/player_utils/test_data/problem_1_from_handout");
 		board.setToPlayNext(Stone.BLACK);
 		MonteCarloGoSolver solver = new MonteCarloGoSolver(board, board.getCellAt(9, 13));
-		System.out.println(board.getCellAt(9, 13).getContent().name());
+		assertEquals(new GoCell(Stone.BLACK, 8, 10), solver.decision());
+	}
+	
+	@Test
+	public void testProblem1FromHandoutTimeLimit() throws FileNotFoundException, CheckFailException, InterruptedException {
+		GoPlayingBoard board = new GoPlayingBoard(workspaceDirectory +
+				"/src/player_utils/test_data/problem_1_from_handout");
+		board.setToPlayNext(Stone.BLACK);
+		MonteCarloGoSolver solver = new MonteCarloGoSolver(board, board.getCellAt(9, 13), 
+				10, System.currentTimeMillis() + 1000 * 10); // Run for 30 seconds
 		assertEquals(new GoCell(Stone.BLACK, 8, 10), solver.decision());
 	}
 }
